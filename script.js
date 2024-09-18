0eccb3194f8da07b49091774ab1393c1c2af9b06
Aniket Singh <aniketsingh3097@gmail.com>
2024-09-18 19:31:10 +0530

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.getElementById('fetchAnimals').addEventListener('click', () => {
-    fetch('/api/animals')
-        .then(response => response.json())
-        .then(data => {
-            const animalList = document.getElementById('animal-list');
-            animalList.innerHTML = data.map(animal => `<p>${animal.name}</p>`).join('');
-        });
-});
